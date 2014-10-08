2b061a2281633a8a87b622a7b58f08a7737a8bdf
Lindsay-R <lrecknell@gmail.com>
2014-10-08 09:47:03 -0600

    1x4 grid frist attempt

--- lib/spiral.rb
+++ lib/spiral.rb
@@ -2,12 +2,18 @@ class Spiral
 
 
   def onebyfour
-    [
-      [1,2,3,4],
-      [12,13,14,5],
-      [11,16,15,6],
-      [10,9,8,7]
-    ]
+
+    num=1
+    width=4
+    grid=[]
+    while num < width do
+     grid << (num..4).to_a
+      num +=1
+    end
+    return grid
+
+
+    
   end
 
 end
