290644c04843a5a29db005369815449a83e16a9c
Lindsay-R <lrecknell@gmail.com>
2014-10-08 10:04:58 -0600

    1x4 grid 3rd attempt, works but not a great solution

--- lib/spiral.rb
+++ lib/spiral.rb
@@ -8,6 +8,8 @@ class Spiral
     grid=[]
 
     grid << (num..width).to_a
+    grid << ((12..14).to_a<< (width+1))
+    grid << ([11, 16, 15]<< (width+2))
     grid << (((num*10)-(width-1))..(num*10)).to_a.reverse
 
 
